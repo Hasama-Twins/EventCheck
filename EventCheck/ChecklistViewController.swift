ccd3ef6f6b42f6515951450a5706d2c35b6e095f
Evelyn Hasama <ehasama11@gmail.com>
2020-11-06 23:29:39 -0800

    More Developments

---
+++ EventCheck/ChecklistViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChecklistViewController.swift
+//  EventCheck
+//
+//  Created by Evelyn Hasama on 11/6/20.
+//
+
+import UIKit
+
+class ChecklistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EventCheck/LocationRevealViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocationRevealViewController.swift
+//  EventCheck
+//
+//  Created by Evelyn Hasama on 11/6/20.
+//
+
+import UIKit
+
+class LocationRevealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
